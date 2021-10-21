89868df3ccb84a8a5a5160242c237bc2244bb9b6
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2021-10-21 21:18:13 +0900

    Allowing non signup user to read diaries

--- app/diaries/queries/getDiary.ts
+++ app/diaries/queries/getDiary.ts
@@ -3,15 +3,11 @@ import db from "db"
 import { z } from "zod"
 
 const GetDiary = z.object({
-  // This accepts type of undefined, but is required at runtime
   id: z.number().optional().refine(Boolean, "Required"),
 })
 
-export default resolver.pipe(resolver.zod(GetDiary), resolver.authorize(), async ({ id }) => {
-  // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+export default resolver.pipe(resolver.zod(GetDiary), async ({ id }) => {
   const diary = await db.diary.findFirst({ where: { id } })
-
   if (!diary) throw new NotFoundError()
-
   return diary
 })
