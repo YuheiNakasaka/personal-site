977ed5cf5cb0082d4320ed334e461b439626c9a0
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2021-11-14 16:43:04 +0900

    Upload image to imgur anonymously

--- app/core/components/LabeledTextArea.tsx
+++ app/core/components/LabeledTextArea.tsx
@@ -4,12 +4,13 @@ import {
   PropsWithoutRef,
   useContext,
   ChangeEvent,
+  useState,
 } from "react"
 import { useField, UseFieldConfig } from "react-final-form"
-
-import { Textarea } from "@chakra-ui/react"
+import { Box, Textarea, Input } from "@chakra-ui/react"
 import { FormControl, FormLabel } from "@chakra-ui/form-control"
 import { FormContext, FormContextType } from "app/diaries/context/FormContext"
+import Resizer from "react-image-file-resizer"
 
 export interface LabeledTextAreaProps extends ComponentPropsWithoutRef<typeof Textarea> {
   /** Field name. */
@@ -26,6 +27,7 @@ export interface LabeledTextAreaProps extends ComponentPropsWithoutRef<typeof Te
 export const LabeledTextArea = forwardRef<HTMLTextAreaElement, LabeledTextAreaProps>(
   ({ name, label, outerProps, fieldProps, labelProps, ...props }, ref) => {
     const context: FormContextType = useContext(FormContext)
+    const [cursorPos, setCursorPos] = useState(context.text.split("\n").length)
     const {
       input,
       meta: { touched, error, submitError, submitting },
@@ -53,9 +55,52 @@ export const LabeledTextArea = forwardRef<HTMLTextAreaElement, LabeledTextAreaPr
             value={context.text}
             onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
               context.setText(e.target.value)
+              setCursorPos(e.target.value.split("\n").length - 1)
             }}
           />
         </FormLabel>
+
+        <div style={{ textAlign: "right" }}>
+          <Box display="inline-block" marginRight="2">
+            <Input
+              type="file"
+              onChange={async (e: ChangeEvent<HTMLInputElement>) => {
+                const data = new FormData()
+                const files = e.target.files
+                if (files !== null) {
+                  const r = new XMLHttpRequest()
+                  const f = files[0]!
+                  Resizer.imageFileResizer(
+                    f,
+                    300,
+                    300,
+                    "JPEG",
+                    100,
+                    0,
+                    (blob: Blob) => {
+                      data.append("type", "file")
+                      data.append("image", blob)
+                      r.open("POST", "https://api.imgur.com/3/image")
+                      r.setRequestHeader("Authorization", `Client-ID 8df2e11f2ef12d6`)
+                      r.onreadystatechange = () => {
+                        if (r.status === 200 && r.readyState === 4) {
+                          let res = JSON.parse(r.responseText)
+                          const url = `https://i.imgur.com/${res.data.id}.png`
+                          const texts = context.text.split("\n")
+                          texts.splice(cursorPos, 0, `![](${url})`)
+                          context.setText(texts.join("\n"))
+                        }
+                      }
+                      r.send(data)
+                    },
+                    "blob"
+                  )
+                }
+              }}
+            />
+          </Box>
+        </div>
+
         {touched && normalizedError && (
           <div role="alert" style={{ color: "red" }}>
             {normalizedError}
