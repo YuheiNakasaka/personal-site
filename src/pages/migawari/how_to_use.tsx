9236c90bb177de8a398a92cb9933d8450bc37620
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2023-06-12 22:13:27 +0900

    スタイルをChakraUIに変更

--- src/pages/migawari/how_to_use.tsx
+++ src/pages/migawari/how_to_use.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import { Avatar, Image } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 const MigawariHowToUsePage: NextPage = () => {
   return (
@@ -12,102 +12,136 @@ const MigawariHowToUsePage: NextPage = () => {
           content="Migawariはしつこい訪問販売や勧誘に困っている一人暮らしの方向けに男性のボイスを提供するアプリです。"
         />
       </Head>
-      <section className="container">
-        <div className="section">
-          <h1>使い方・よくある質問</h1>
+      <Flex>
+        <Box p={"10px"} maxW={"600px"}>
+          <Text fontSize={"1.5rem"} fontWeight={"bold"}>
+            使い方・よくある質問
+          </Text>
 
-          <section className="how-to-section">
-            <h2># ボイスを再生するには？</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # ボイスを再生するには？
+            </Text>
             <p>
               使いたいボイスをクリックするだけです。ボイスが再生し終わるまで別のボイスは再生できないので気をつけてください。
             </p>
-          </section>
+          </Box>
 
-          <section className="how-to-section">
-            <h2># お気に入りとは？</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # お気に入りとは？
+            </Text>
             <p>よく使うボイスを「お気に入り」ページに保存する機能です。</p>
             <p>
               ホームには沢山ボイスがあるのでよく利用するボイスはお気に入りに追加しておくと便利です。
             </p>
-          </section>
+          </Box>
 
-          <section className="how-to-section">
-            <h2># お気に入りボイスを並べ替えたい</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # お気に入りボイスを並べ替えたい
+            </Text>
             <p>
               お気に入りに入れたボイスの右端のハシゴアイコンを長押しすると上下好きな場所に移動できます。
             </p>
-            <div className="video-section">
+            <Flex
+              w={"100%"}
+              flexDirection={"column"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              m={"1rem 0 4rem"}
+            >
               <video width="50%" controls>
                 <source
                   src="../../how_to_sort_favorite_voice_res.mp4"
                   type="video/mp4"
                 />
               </video>
-            </div>
-          </section>
+            </Flex>
+          </Box>
 
-          <section className="how-to-section">
-            <h2># ほしいボイスや機能が無い</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # ほしいボイスや機能が無い
+            </Text>
             <p>
               <a
                 href="https://docs.google.com/forms/d/e/1FAIpQLSfpzn42RJALUdumkXfz-WfBiPS1cLQHsEnT9eoULMU99QEZfg/viewform?usp=sf_link"
                 target="_blank"
                 rel="noreferrer"
+                style={{ color: "#3182ce" }}
               >
                 リクエストフォーム
               </a>
               からリクエストしてください。
             </p>
-          </section>
+          </Box>
 
-          <section className="how-to-section">
-            <h2># ネットがなくても使える？</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # ネットがなくても使える？
+            </Text>
             <p>はい。インターネット接続なしでボイスを再生できます。</p>
-          </section>
+          </Box>
 
-          <section className="how-to-section">
-            <h2># 広告を入れる予定はある？</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # 広告を入れる予定はある？
+            </Text>
             <p>
               いいえ。広告を入れるといざ使いたいと思った時にスムーズに使えなくて不便だと思うので入れる予定はありません。
             </p>
-          </section>
+          </Box>
 
-          <section className="how-to-section">
-            <h2># 友人に紹介したい</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # 友人に紹介したい
+            </Text>
             <p>
               アプリのホームの右上の[歯車アイコン]をクリックして表示される[応援する
               📣]という文字を押していただくとアプリのレビューやシェア用のボタンが表示されます。
             </p>
             <p>
               また、
-              <a href="/migawari/about" target="_blank" rel="noreferrer">
+              <a
+                href="/migawari/about"
+                target="_blank"
+                rel="noreferrer"
+                style={{ color: "#3182ce" }}
+              >
                 こちらのアプリ紹介ページ
               </a>
               のURLをシェアしていただくのも良いかもしれないです。
             </p>
-          </section>
+          </Box>
 
-          <section className="how-to-section">
-            <h2># 利用規約/プライバシーポリシーはどこにありますか？</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # 利用規約/プライバシーポリシーはどこにありますか？
+            </Text>
             <p>
               <a
                 href="/migawari/migawari_privacy_policy"
                 target="_blank"
                 rel="noreferrer"
+                style={{ color: "#3182ce" }}
               >
                 こちら
               </a>
               です。
             </p>
-          </section>
+          </Box>
 
-          <section className="how-to-section">
-            <h2># 誰が作っているの？</h2>
+          <Box mt={"2rem"}>
+            <Text fontSize={"1.2rem"} fontWeight={"bold"}>
+              # 誰が作っているの？
+            </Text>
             <p>
               <a
                 href="https://twitter.com/razokulover"
                 target="_blank"
                 rel="noreferrer"
+                style={{ color: "#3182ce" }}
               >
                 @Yuhei Nakasaka
               </a>
@@ -116,55 +150,9 @@ const MigawariHowToUsePage: NextPage = () => {
             <p>
               元々は一人暮らしで困っていた友人向けに作っただけのアプリです。使いにくい点も多々あるかと思いますが広告無しでボランティアとして作っているので温かい目で見ていただけると幸いです。
             </p>
-          </section>
-        </div>
-      </section>
-      <style global jsx>{`
-        h1,
-        h2,
-        p {
-          font-weight: bold;
-        }
-        h1 {
-          font-size: 1.5rem;
-          padding: 1rem 0 0 0;
-        }
-        h2 {
-          font-size: 1.2rem;
-          padding: 1rem 0 0 0;
-        }
-        p {
-          font-weight: normal;
-          padding: 8px 0 0 0;
-        }
-        a {
-          color: #3182ce;
-        }
-        .container {
-          min-height: 100vh;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        .section {
-          max-width: 600px;
-          padding: 10px;
-        }
-        .how-to-section {
-          margin-top: 2rem;
-        }
-        .video-section {
-          width: 100%;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          margin: 1rem 0 4rem;
-        }
-        video {
-          margin-top: 1rem;
-        }
-      `}</style>
+          </Box>
+        </Box>
+      </Flex>
     </>
   );
 };
